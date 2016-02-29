baaaf9339933a864aac152be6fe175fcbe2a23e9
Kaylie DeHart <kaylie.dehart@gmail.com>
2016-02-29 18:17:44 -0500

    changed to STDIN, using JSONDecoder

--- 7/Streaming/xstream.py
+++ 7/Streaming/xstream.py
@@ -3,30 +3,55 @@ import sys
 import json
 
 class EchoStream:
+	buf = None
+
 	def __init__(self):
+		self.buf = None
 		self.main()
 
-	def validate(self):
+	def nonatomic(self, frag):
+		return frag
+
+	def validate(self, frag):
 		"""
-		Validate input as correct JSON. Return True if it follows JSON specs, False if not.
+		Validate input as correct JSON. 
+		If there are multiple JSON entities separated by a space, evaluate them separately.
+		If there is an incomplete JSON array or object, buffer the fragment and wait to see
+		if the object or array completes.
+		Return a list of the valid JSON fragments passed in, or False.
 		"""
-		pass
+		valid_input = []
+		try:
+			fragment = json.JSONDecoder().decode(frag)
+			valid_input.append(fragment)
+			return valid_input
+		except ValueError:
+			# In case of arrays and objects, check if they're incomplete, and if so, buffer
+			# corner case: 4{} should be 4 {}
+			if ("{" or "[") in frag:
+				self.nonatomic(frag)
+			else:
+				return False
 
 	def main(self):
 		"""
 		Get input from stdin, validate, and then echo. When done, return the number of 
 		messages received.
 		"""
-		valid_json = True
 		msg_count = {"count": 0}
-		while valid_json:   
-			fragment = raw_input()  
-			if self.validate(fragment):
-				print fragment  
-			else:
-				json.dumps[msg_count, sys.stdout]
-		sys.exit()
+		while True:
+			try:
+				line = sys.stdin.readline()
+				echo_frag = self.validate(line)  
+				if echo_frag:
+					for echo in echo_frag:
+						print echo	
+						msg_count["count"] += 1
+			except KeyboardInterrupt or EOFError:
+				print msg_count
+				sys.exit()
 		
+
 
 if __name__ == "__main__":
 	EchoStream()
